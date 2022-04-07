546280643bb295c17748cb94fffe67c20a361f78
chagasmaia <chagas.maia@lojaodobras.com.br>
2022-04-07 18:17:53 -0300

    1 commit

---
+++ index.js
@@ -0,0 +1,8 @@
+
+ var valmoco = document.getElementById('almoco');
+ var vemail = document.getElementById('email');
+
+// Metodo para mostrar alert
+function alerta(){
+	alert("Formulario enviado com sucesso" + valmoco.value + " - " + vemail.value);
+}
